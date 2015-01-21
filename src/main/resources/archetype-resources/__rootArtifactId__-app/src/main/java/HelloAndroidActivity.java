1040d100020af12deccdb519289be76683a014dd
allenz8512 <allenz8512@gmail.com>
2015-01-21 16:58:47 +0800

    add a test module

--- src/main/resources/archetype-resources/__rootArtifactId__-app/src/main/java/HelloAndroidActivity.java
+++ src/main/resources/archetype-resources/__rootArtifactId__-app/src/main/java/HelloAndroidActivity.java
@@ -6,12 +6,20 @@ package ${package};
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class HelloAndroidActivity extends ActionBarActivity {
+public class HelloAndroidActivity extends ActionBarActivity implements
+		OnClickListener {
+
+	private Button helloWorldButton;
+	private TextView messageText;
 
 	/**
 	 * Called when the activity is first created.
-	 * 
+	 *
 	 * @param savedInstanceState
 	 *            If the activity is being re-initialized after previously being
 	 *            shut down then this Bundle contains the data it most recently
@@ -23,6 +31,9 @@ public class HelloAndroidActivity extends ActionBarActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		getSupportActionBar().show();
+		helloWorldButton = (Button) findViewById(R.id.button_helloworld);
+		helloWorldButton.setOnClickListener(this);
+		messageText = (TextView) findViewById(R.id.text_message);
 	}
 
 	@Override
@@ -30,6 +41,13 @@ public class HelloAndroidActivity extends ActionBarActivity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public void onClick(final View v) {
+		if (v == helloWorldButton) {
+			messageText.setText(R.string.hit_text2);
+		}
 	}
 
 }
